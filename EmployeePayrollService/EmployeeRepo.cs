850eb2a2c7ce948f8440ff88ffcc8bf05d7e5f29
victor-singha <vicunite2@gmail.com>
2021-05-30 20:06:20 +0530

    remove comment

--- EmployeePayrollService/EmployeeRepo.cs
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -61,55 +61,6 @@ namespace EmployeePayrollService
                 con.Close();
             }
         }
-        //public void GetAllEmployee_With_Thread()
-        //{
-        //    SqlConnection con = new SqlConnection(connectionString);
-        //    try
-        //    {
-        //        EmployeeModel employeeModel = new EmployeeModel();
-        //        using (con)
-        //        {
-        //            string query = @"Select * from employee_payroll;";
-        //            SqlCommand cmd = new SqlCommand(query, con);
-        //            con.Open();
-        //            SqlDataReader dr = cmd.ExecuteReader();
-        //            if (dr.HasRows)
-        //            {
-                        
-        //                while (dr.Read())
-        //                {
-        //                    employeeModel.EmployeeID = dr.GetInt32(0);
-
-        //                    employeeModel.EmployeeName = dr.GetString(1);
-        //                    employeeModel.BasicPay = dr.GetDecimal(2);
-        //                    employeeModel.StartDate = dr.GetDateTime(3);
-        //                    employeeModel.Gender = Convert.ToChar(dr.GetString(4));
-        //                    //employeeModel.PhoneNumber = dr.GetString(5);
-        //                    //employeeModel.Address = dr.GetString(6);
-        //                    employeeModel.Department = dr.GetString(7);
-        //                    //employeeModel.Deductions = dr.GetDouble(8);
-        //                    //employeeModel.TaxablePay = dr.GetDouble(9);
-        //                    //employeeModel.Tax = dr.GetDouble(10);
-        //                    //employeeModel.NetPay = dr.GetDouble(11);
-        //                    Console.WriteLine(employeeModel.EmployeeName + " " + employeeModel.BasicPay + " " + employeeModel.StartDate + " " + employeeModel.Gender + " " + employeeModel.PhoneNumber + " " + employeeModel.Address + " " + employeeModel.Department + " " + employeeModel.Deductions + " " + employeeModel.TaxablePay + " " + employeeModel.Tax + " " + employeeModel.NetPay);
-        //                    Console.WriteLine("\n");
-        //                }
-        //            }
-        //            else
-        //            {
-        //                Console.WriteLine("No data found");
-        //            }
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        System.Console.WriteLine(e.Message);
-        //    }
-        //    finally
-        //    {
-        //        con.Close();
-        //    }
-        //}
         public bool AddEmployee(EmployeeModel model)
         {
             try
